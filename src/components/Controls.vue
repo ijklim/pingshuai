dbac30d8e3b78d5f62d652cf81af8f4195692a6b
Ivan Lim <aimprove@gmail.com>
2017-08-31 10:59:38 -0400

    Set big Play button in the middle on start of app

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class='controls'>
-    <transition name='fade' mode="out-in">
+  <div class='controls' :class='{ "started": started }'>
+    <transition name='fade' mode='out-in'>
       <!-- Note: Without at least 1 :key binding transition would not work, and it should correspond to a variable without generating Vue warning -->
       <button @click='toggle' class='pause' :key='active' v-if='active' title='Pause'></button>
       <button @click='toggle' class='play' v-else title='Start'></button>
@@ -23,12 +23,17 @@ export default {
   // variables
   data () {
     return {
+      started: false
     }
   },
 
   // methods
   methods: {
     toggle () {
+      if (!this.started) {
+        // Starting for the first time, need to move controls down
+        this.started = true
+      }
       this.$emit('play-button-toggle')
     }
   },
@@ -42,22 +47,42 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .controls {
-  position: absolute;
-  width: 100%;
-  bottom: 0px;
+  /* width: 100%; */
   text-align: center;
-  margin-bottom: 10px;
+  /* Center element */
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.started {
+  /* Bottom of screen */
+  /* position: absolute; */
+  /* bottom: 0px;
+  margin-bottom: 10px; */
+  transition: all .75s ease-in-out;
+  transform: translate(-50%, 42vh);
 }
 
 button {
-  width: 50px;
-  height: 50px;
+  width: 256px;
+  height: 256px;
   border: none;
   outline: none;
   background: none;
 }
 
+.started button {
+  width: 50px;
+  height: 50px;
+}
+
 button.play {
+  background-image: url('../assets/icon-play-256x256.png');
+}
+
+.started button.play {
   background-image: url('../assets/icon-play-50x50.png');
 }
 
